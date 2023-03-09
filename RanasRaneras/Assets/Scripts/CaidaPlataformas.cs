07ba27c54bc9e6110836027ee1973abf94ebf2b0
AlyceT13 <125464722+AlyceT13@users.noreply.github.com>
2023-03-09 20:30:05 +0100

    Plataformas que caen
    Hay un prefab de cubo que son las plataformas que se crean y un prefab que es el controlador de creación de plataformas que caen, este tiene una función que instancia las plataformas y varios temporizadores que controlan la creación de plataformas y que según avanza el juego cada vez se crean más plataformas en menos tiempo.

---
+++ RanasRaneras/Assets/Scripts/CaidaPlataformas.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaidaPlataformas : MonoBehaviour
+{
+    [SerializeField] GameObject prefabPlataforma;
+    [SerializeField] float limiteInferior = -8.82f, limiteSuperior = 8.8f;
+    [SerializeField] float posicionY=8;
+    [SerializeField] float valorActualTimer;
+    [SerializeField] float timer = 2f;
+    float valorInicialTimer2;
+    float timer2 = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        valorActualTimer=timer;
+        valorInicialTimer2 = timer2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timer2 -= Time.deltaTime;
+        if (timer2 <= 0)
+        {
+            if(valorActualTimer > 0.5) 
+            { 
+                valorActualTimer -= 0.5f;
+                timer2 = valorInicialTimer2;
+            }
+            else 
+            { 
+                valorActualTimer = 0.5f; 
+                timer2 = valorInicialTimer2; 
+            }
+        }
+        if (timer <= 0)
+        {
+            CrearPlataforma();
+            timer = valorActualTimer;
+        }
+    }
+    void CrearPlataforma()
+    {
+        GameObject clon = Instantiate(prefabPlataforma);
+        float x = Random.Range(limiteInferior, limiteSuperior);
+        clon.transform.position = new Vector3(x, posicionY, 0);
+    }
+   
+}
