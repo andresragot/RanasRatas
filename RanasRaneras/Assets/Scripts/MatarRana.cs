5c0e70a509f45ae125ba3924bff42c2ce76840d5
andresragot <62946319+andresragot@users.noreply.github.com>
2023-03-18 13:33:11 +0100

    Arregladas algunas cosas del QA

--- RanasRaneras/Assets/Scripts/MatarRana.cs
+++ RanasRaneras/Assets/Scripts/MatarRana.cs
@@ -8,15 +8,19 @@ public class MatarRana : MonoBehaviour
     Transform ranaCheck;
     [SerializeField]
     LayerMask ranaLayer;
+    [SerializeField]
+    LayerMask ground;
 
     BoxCollider2D box;
+    Rigidbody2D rb;
 
-    RaycastHit2D raycast;
+    RaycastHit2D raycast, raycastGround;
 
     // Start is called before the first frame update
     void Start()
     {
         box = GetComponent<BoxCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -24,11 +28,21 @@ public class MatarRana : MonoBehaviour
     {
         if (DetectarRana())
         {
-            Debug.Log(raycast.collider.tag);
             if(raycast.collider!=null && raycast.collider.tag == "Rana")
             {
+                if(rb.constraints == RigidbodyConstraints2D.FreezeAll)
+                {
+                    return;
+                }
                 Corazones cora = raycast.collider.GetComponent<Corazones>();
                 cora.Vida--;
+            }
+        }
+        else if (DetectarSuelo())
+        {
+            if (raycastGround.collider.gameObject != this && raycastGround.collider != null)
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezeAll;
             }
         }
     }
@@ -37,5 +51,11 @@ public class MatarRana : MonoBehaviour
     {
         raycast = Physics2D.BoxCast(ranaCheck.position, box.bounds.size, 0, Vector2.down, 1, ranaLayer);
         return raycast;
+    }
+
+    private bool DetectarSuelo()
+    {
+        raycastGround = Physics2D.Raycast(ranaCheck.position, Vector2.down, 0.1f, ground);
+        return raycastGround;
     }
 }
